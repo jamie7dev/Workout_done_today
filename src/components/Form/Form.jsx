74c4fd4383421a510c72cbf8c895922ba085cf3f
jeongseoe <gldi1633@naver.com>
2022-09-03 19:07:44 +0900

    수정

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,34 +1,78 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
+
+
 
 const Form = () => {
-    const [input, setInput] = useState({ id: "", pw: "", pwConfirm: "" });
+    const [input, setInput] = useState({
+        username: "",
+        password: "",
+        passwordConfirm: ""
+    });
+
+    const timestamp = new Date().getTime();
+
+    const addHandler = () => {
+        const { username, password } = input;
+        const annoyance = {
+            id: +timestamp,
+            username: username,
+            password: password
+        };
+        axios.post("http://localhost:3001/data", annoyance);
+    };
 
     const inputHandler = (e) => {
         const { name, value } = e.targer;
         setInput({ ...input, [name]: value });
-    }
+    };
+
+
+
 
     return (
         <ContainerWrap>
             <StTitle>
                 운동을 시작해보자구,  동지!!!
             </StTitle>
-            <InputWrap>
-                <StLabel>아이디</StLabel>
-                <StInputId placeholder="아이디를 입력하세요." />
-                <StButton>중복확인</StButton>
-            </InputWrap>
-            <StSmallLabel>* 아이디는 영어와 숫자로 6자이상 8자 이하로 입력해주세요. *</StSmallLabel>
-            <InputWrap>
-                <StLabel >비밀번호</StLabel>
-                <StInput placeholder="비밀번호를 입력하세요." />
-            </InputWrap>
-            <StSmallLabel style={{ marginLeft: "50px" }}>* 비밀번호는 영어, 숫자와 특수문자포함 8자이상 20자이하로 입력해주세요 *</StSmallLabel>
-            <InputWrap>
-                <StLabel>비밀번호 재확인</StLabel>
-                <StInput placeholder="비밀번호를 재입력하세요." />
-            </InputWrap>
+            <StForm>
+                <InputWrap >
+                    <StLabel>아이디</StLabel>
+                    <StInputId placeholder="아이디를 입력하세요."
+                        onChange={inputHandler}
+                        type="text"
+                        name="username"
+                        id="username"
+                        value={input.username} />
+                    {/* <StButton>중복확인</StButton> */}
+                </InputWrap>
+                <StSmallLabel>* 아이디는 영어와 숫자로 6자이상 8자 이하로 입력해주세요. *</StSmallLabel>
+
+                <InputWrap >
+                    <StLabel >비밀번호</StLabel>
+                    <StInput placeholder="비밀번호를 입력하세요."
+                        onChange={inputHandler}
+                        type="passWord"
+                        name="passWord"
+                        id="passWord"
+                        value={input.password} />
+                </InputWrap>
+                <StSmallLabel style={{ marginLeft: "50px" }}>* 비밀번호는 영어, 숫자와 특수문자포함 8자이상 20자이하로 입력해주세요 *</StSmallLabel>
+                <InputWrap >
+                    <StLabel>비밀번호 재확인</StLabel>
+                    <StInput placeholder="비밀번호를 재입력하세요."
+                        onChange={inputHandler}
+                        type="password"
+                        name="passWordConfirm"
+                        id="passWordConfirm"
+                        value={input.passwordConfirm} />
+                </InputWrap>
+                <JoinBtn type="button"
+                    onClick={() => { addHandler(); console.log(input); }}>
+                    운동하러 가자
+                </JoinBtn>
+            </StForm>
         </ContainerWrap>
     );
 };
@@ -105,4 +149,22 @@ const StSmallLabel = styled.label`
   color: #285999;
   font-weight: 600;
   height: 20vw;
+`;
+
+const JoinBtn = styled.button`
+  width: 150px;
+  height: 40px;
+  margin-left: 10px;
+  margin-top: 3%;
+
+  font-size: 15px;
+  background-color: #4B89DC;
+  color: black;
+  font-weight: 600;
+  border-radius: 10px;
+`;
+
+
+const StForm = styled.form`
+    
 `;
