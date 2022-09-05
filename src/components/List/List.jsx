298e8e1435d6972255e8ad5b88e69cf713e4edc8
jeongseoe <gldi1633@naver.com>
2022-09-05 20:37:33 +0900

    card배열이 가로로 되서 줄바꿈 시행해야함..찾는중

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,41 +2,50 @@ import React from "react";
 import styled from "styled-components";
 import Card from "./Card";
 import useFetchPost from "./useFetchPost";
+import { useNavigate } from "react-router-dom";
 
 const List = () => {
-    const posts = useFetchPost(null);
-    return (
-        <>
-            <div>
-                <PostTitle>물론이지, 너도 ?</PostTitle>
-                <StListContainer>
-                    <StCard>
-                        {posts?.map((post) => {
-                            // kry=1 post=json{}
-                            return <Card key={post.id} post={post} />
-                        })}
-                    </StCard>
-                </StListContainer>
-            </div>
+  const posts = useFetchPost(null);
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>
+        <PostTitle >
+          물론이지, 너도 ?
+          <StButton onClick={() => {
+            navigate(`/post`);
+          }}>
+            👉인증가실?👈
+          </StButton>
+        </PostTitle>
+        <StListContainer>
+          <StCard>
+            {posts?.map((post) => {
+              // kry=1 post=json{}
+              return <Card key={post.id} post={post} />
+            })}
+          </StCard>
+        </StListContainer>
+      </div>
 
-        </>
-    );
+    </>
+  );
 };
 
 export default List;
 
 const StListContainer = styled.div`
   background-color: whitesmoke;
-  width: 100%;
+  width: 100vw;
   height: 100%;
   margin: auto;
-  display: flex;
+
 `;
 
 const StCard = styled.div`
-  display: flex;
   margin: auto;
   padding: 20px;
+  display: inline-flexbox;
 `;
 
 const PostTitle = styled.div`
@@ -48,4 +57,22 @@ const PostTitle = styled.div`
   height: 60px;
   background-color: whitesmoke;
   width: 100%
+`;
+
+const StButton = styled.button`
+  margin-left:60px ;
+  font-size: 30px;
+  font-weight: 600;
+  border-radius: 10px;
+  background-color: whitesmoke;
+  border: none;
+  text-decoration: underline;
+  :hover{
+    background-color: #f2f294;
+    border: 4px solid #4B89DC;
+    text-decoration: none;
+  }
+  :touch-action{
+    font-size: 15px;
+  }
 `;
