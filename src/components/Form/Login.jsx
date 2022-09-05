2019f2507bf8ada8d337d013196a278ffdd6de01
jamie7dev <jamie7dev@gmail.com>
2022-09-05 22:33:50 +0900

    다시 커밋

--- src/components/Form/Login.jsx
+++ src/components/Form/Login.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import axios from "axios";
 import { useDispatch } from "react-redux";
-import { login, __login } from "../../redux/modules/userSlice";
+import { __login } from "../../redux/modules/userSlice";
 
 
 const Login = () => {
