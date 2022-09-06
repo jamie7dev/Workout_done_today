f36dd974346458c6523461cef2c6d49fe3c5c77f
jeongseoe <gldi1633@naver.com>
2022-09-07 03:36:09 +0900

    style 변경

--- src/components/Detail/DetailCard.jsx
+++ src/components/Detail/DetailCard.jsx
@@ -3,18 +3,19 @@ import styled from "styled-components";
 
 const DetailCard = (props) => {
 
-    console.log(props);
     return (
         <>
             <StHeader>👊 당연한거 아니겠냐고 👊</StHeader>
+
             <StContainer>
                 <StImgBox>
                     {props.post.imageUrl && (
                         <img
                             src={props.post.imageUrl}
+                            width="45%"
+                            height="45%"
                             alt="preview-img"
-                            width="70%"
-                            height="80%"
+
                             style={{
                                 border: "2px solid white",
                                 borderRadius: "10px",
@@ -26,19 +27,25 @@ const DetailCard = (props) => {
                 </StImgBox>
                 <StContentBox>
                     <StContent>
-                        <p style={{ marginRight: "10px", color: "white" }}>작성자 : </p>
-                        <p> {props.post.author}</p>
+                        <p style={{ marginRight: "10px", color: "white", padding: "5px" }}> 작성자 : </p>
+                        <p style={{ backgroundColor: "white", borderRadius: "5px", padding: "5px" }}> {props.post.author}</p>
                     </StContent>
                     <StContent>
-                        <p style={{ marginRight: "10px", color: "white" }}>제목 :</p>
-                        <p> {props.post.title}</p>
+                        <p style={{ marginRight: "10px", color: "white", padding: "5px" }}> 제목 :</p>
+                        <p style={{ backgroundColor: "white", borderRadius: "5px", padding: "5px" }}> {props.post.title}</p>
                     </StContent>
                     <StContent>
-                        <p style={{ marginRight: "10px", color: "white" }}>내용 :</p>
-                        <p style={{}}>{props.post.content}</p>
+                        <p style={{ marginRight: "10px", color: "white", padding: "5px" }}> 내용 :</p>
+                        <p style={{ backgroundColor: "white", borderRadius: "5px", padding: "5px" }}>{props.post.content}</p>
                     </StContent>
                 </StContentBox>
+                <StBtnBox>
+                    <StButton>수정하기</StButton>
 
+                    <StButton>삭제하기</StButton>
+
+                    <StButton>또 인증하기!</StButton>
+                </StBtnBox>
             </StContainer>
         </>
     );
@@ -49,42 +56,58 @@ export default DetailCard;
 const StContainer = styled.div`
   text-align: center;
   margin-top: 30px;
-  background-color: #4B89DC;
-  width: 50%;
+  background-color: #8ebefd;
+  width: 40%;
+  height: auto;
   border-radius: 30px;
   margin-left:auto;
   margin-right: auto;
-  height: 50%;
+  max-height: 50vw;
   margin-bottom: 30px;
-  margin-top: 30px;
+  margin-top: 20px;
   padding-top: 20px;
 `;
 
 const StImgBox = styled.div`
   text-align: center;
-  margin-bottom: 30px;
-    
+  margin-bottom: 10px;
+  max-height: 60vw;
 `;
 
 const StContent = styled.div`
   text-align: center;
-  font-size: 30px;
+  font-size: 20px;
   display: flex;
   justify-content: center;
 `;
 
 const StContentBox = styled.div`
+//비어져있어도 상관없음
 `;
 
 const StHeader = styled.div`
   margin  : auto;
   font-size: 30px;
   text-align: center;
-  margin-top: 30px;
-  padding: 1%;
+  margin-top: 10px;
+  padding: 0.5%;
+  padding-bottom: 20px;
   width:30%;
   height: 30px;
   background-color: whitesmoke;
   border-radius: 15px;
   border: 5px solid #4B89DC;
+`;
+
+const StBtnBox = styled.div`
+  justify-content: center;
+`;
+
+const StButton = styled.button`
+  border: none;
+  background-color: #8ebefd;
+  font-size: 20px;
+  margin: 10px;
+  margin-right: 15px;
+  text-align: center;
 `;
